60b93166bf3ce567c8bdf58f979a9930aa270966
bellamy <biznes2001md@gmail.com>
2026-07-17 09:46:19 +0300

    Initial Commit

---
+++ Reson/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Reson
+//
+//  Created by ramsyyy tt on 7/17/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ ResonTests/ResonTests.swift
@@ -0,0 +1,19 @@
+//
+//  ResonTests.swift
+//  ResonTests
+//
+//  Created by ramsyyy tt on 7/17/26.
+//
+
+import Testing
+@testable import Reson
+
+struct ResonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
